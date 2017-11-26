a797c2baf1787acd19225d6d725b71e1dd22fb5a
cyamonide <cyamonide@gmail.com>
2017-11-26 16:58:52 -0500

    Create USACO wormhole solution

---
+++ USACO/Chapter 1/Section 1.4/wormhole/wormhole.cpp
@@ -0,0 +1,15 @@
+// Created:	Novmember 14, 2017
+// Author:	Simon Liu
+// Problem:	Wormhole
+// Section:	1.4
+// Completed:
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+	return 0;
+}
