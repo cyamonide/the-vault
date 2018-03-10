4e18a9fce8b573e39eb59b45044feb90e2b2099d
cyamonide <cyamonide@gmail.com>
2018-03-09 21:46:43 -0500

    Create ECOO folders

---
+++ DMOJ/ecoo/ecoo13r1p4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <fstream>
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+typedef long long ll;
+
+using namespace std;
+
+struct Foobar {
+    int foobat = 0;
+};
+
+int main() {
+    ifstream fin;
+    fin.open("DATA11.txt")
+
+    return 0;
+}
---
+++ ECOO 2018/test/test.py
@@ -0,0 +1,33 @@
+'''
+ECOO Python 2 Template
+By: Andrew Wang
+
+Intended usage is to use the comments as guidelines 
+rather than working lines of code in the program. 
+
+You should probably delete all of the comments you don't
+need so as to not cluster fuck your program.
+'''
+import math, itertools, time
+
+with open("DATA00.in") as input:
+# SPECIFICATION INT'S IN-LINE
+    # a, b, c = [int(x) for x in next(input).split(' ')]
+
+# DATA LIST IN-LINE 
+    # arr = list(map(int, next(input).split(' ')))
+
+# CONTINUOUS INPUT UNTIL SENTINEL_VALUE
+# (ENSURE THAT THE SENTINEL IS STRING TYPED)
+    '''
+    found = False
+    while not found:
+        s = next(input)
+        if s == 
+0
+:
+            found = True
+        else:
+            ##### ALGORITHM CODE HERE
+    '''
+    input.close()
